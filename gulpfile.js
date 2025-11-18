f4aa9b97e6f1f67a9c88a85537a1fd448e8f6c52
Adam Tilley <admin@Adams-M1.local>
2025-11-18 18:08:57 -0500

    Add automated build and deployment workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,23 @@
 const {src, dest} = require('gulp');
-const pump = require('pump');
-const zip = require('gulp-zip');
-const beeper = require('beeper');
+const gulpZip = require('gulp-zip');
 
-function handleError(done) {
-    return function (err) {
-        if (err) {
-            console.error('Error details:', err);
-            beeper();
-        }
-        return done(err);
-    };
-}
-
-function zipper(done) {
+function zipper() {
     const filename = require('./package.json').name + '.zip';
 
-    pump([
-        src([
-            '**',
-            '!node_modules', '!node_modules/**',
-            '!dist', '!dist/**',
-            '!.git', '!.git/**',
-            '!.github', '!.github/**',
-            '!yarn-error.log',
-            '!yarn.lock',
-            '!deploy.sh',
-            '!gulpfile.js',
-            '!.DS_Store',
-            '!*.zip'
-        ]),
-        zip(filename),
-        dest('dist/')
-    ], handleError(done));
+    return src([
+        '**',
+        '!node_modules', '!node_modules/**',
+        '!dist', '!dist/**',
+        '!.git', '!.git/**',
+        '!.github', '!.github/**',
+        '!yarn-error.log',
+        '!deploy.sh',
+        '!gulpfile.js',
+        '!.DS_Store',
+        '!*.zip'
+    ])
+    .pipe(gulpZip(filename))
+    .pipe(dest('dist/'));
 }
 
 exports.zip = zipper;
